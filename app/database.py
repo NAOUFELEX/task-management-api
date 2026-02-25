45eb7c23864c39da23de999a26c90477160bad97
NAOUFELEX <kellilnaoufel@gmail.com>
2026-02-24 16:17:23 -0800

    Add FastAPI Entry Point

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL = "mysql+pymysql://root:lol2002@localhost:3306/taskdb"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
